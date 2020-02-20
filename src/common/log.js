43916537f61af93339eca01535a14dbcfc1c9b0c
Lam <daibanglam@gmail.com>
2020-02-20 17:04:58 +0700

    Update log format

--- src/common/log.js
+++ src/common/log.js
@@ -1,26 +1,43 @@
 const winston = require('winston');
 
+const {
+  combine,
+  colorize,
+  splat,
+  printf,
+  timestamp: timestampFormat,
+} = winston.format;
+
+const logColors = {
+  warn: 'yellow',
+  info: 'green',
+  debug: 'magenta',
+  error: 'red',
+};
+
+// custom log format that add current date time
+const withTimeFormat = printf(({ level, message, timestamp }) => {
+  const tm = new Date(timestamp);
+  return `${tm.toString()} ${level}: ${message}`;
+});
+
+// list of chained formats
+const formats = [
+  process.env.NODE_ENV === 'dev' ? colorize({ colors: logColors }) : null,
+  timestampFormat(),
+  splat(),
+  withTimeFormat,
+].filter((e) => !!e);
+
+// winston logger instance
 const logger = winston.createLogger({
   transports: [
-    // log all levels data to console
-    // levels: error=0, warn=1, info=2, verbose=3, debug=4, silly=5
     new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(),
-        winston.format.splat(),
-        winston.format.simple(),
-      ),
+      // define how the log was displayed
+      format: combine(...formats),
+      // log all levels data to console
+      // levels: error=0, warn=1, info=2, verbose=3, debug=4, silly=5
       level: 'silly',
-      // customize color
-      levels: {
-        levels: 'winston.config.npm',
-        colors: {
-          warn: 'purple',
-          info: 'green',
-          debug: 'yellow',
-          error: 'red',
-        },
-      },
     }),
   ],
 });
